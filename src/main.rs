cc6149a7007807fb1eb4ffdc798fb0bd452d6d1e
Anise <cameron.casebolt17@gmail.com>
2025-08-20 19:21:48 -0700

    kinda sorata pulled file reading into its own function need to removed old code and integrate

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
-use std::fs;
-use std::io::Read;
-use std::string;
+use std::fmt::Result;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 fn main() {
     let mut base_list = match fs::File::open("wordleanswers.txt") {
         Ok(f) => f,
         Err(e) => panic!("error opening file {} {}!", e, e),
     };
-    let mut contents = String::new();
-    match base_list.read_to_string(&mut contents) {
-        Ok(_) => println!("{}", contents),
-        Err(e) => panic!("cannot read file{}", e),
-    };
+    let mut contents: Vec<String> = Vec::new();
+    let reader = BufReader::new(base_list);
+    contents = reader.lines().collect::<Result<Vec<String>, std::Error>>();
+    let mut refined = contents.iter();
+
+    refined.filter(|word| word.contains("a")).collect();
+    for item in refined {
+        println!("{}", item);
+    }
+}
+fn read_lines<P>(file_path: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(file_path)?;
+    Ok(io::BufReader::new(file).lines())
 }
