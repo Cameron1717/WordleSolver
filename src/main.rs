b54f77a9e6be805c87ef6f2b3c4904e63116525f
Anise <cameron.casebolt17@gmail.com>
2025-08-20 18:36:15 -0700

    added file read functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::fs;
+use std::io::Read;
+use std::string;
 fn main() {
-    println!("Hello, world!");
+    let mut base_list = match fs::File::open("wordleanswers.txt") {
+        Ok(f) => f,
+        Err(e) => panic!("error opening file {} {}!", e, e),
+    };
+    let mut contents = String::new();
+    match base_list.read_to_string(&mut contents) {
+        Ok(_) => println!("{}", contents),
+        Err(e) => panic!("cannot read file{}", e),
+    };
 }
