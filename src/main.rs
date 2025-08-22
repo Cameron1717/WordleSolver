20fae5d280c4748627ad7bac01b0370e7ffb0fd1
Anise <cameron.casebolt17@gmail.com>
2025-08-22 16:00:53 -0700

    rewrote file aprser, basic filtering

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,18 @@ use std::fmt::Result;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+const DEFAULT_FILE: &str = "wordleanswers.txt";
 fn main() {
-    let mut base_list = match fs::File::open("wordleanswers.txt") {
-        Ok(f) => f,
-        Err(e) => panic!("error opening file {} {}!", e, e),
-    };
-    let mut contents: Vec<String> = Vec::new();
-    let reader = BufReader::new(base_list);
-    contents = reader.lines().collect::<Result<Vec<String>, std::Error>>();
-    let mut refined = contents.iter();
-
-    refined.filter(|word| word.contains("a")).collect();
-    for item in refined {
-        println!("{}", item);
+    let reader = read_lines(DEFAULT_FILE).unwrap_or_else(|err| {
+        panic!("error occured {}", err);
+    });
+    let mut filtered = reader.into_iter();
+    let result = filtered.filter(|word| !word.contains("a"));
+    for word in result {
+        println!("{}", word);
     }
 }
-fn read_lines<P>(file_path: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
+fn read_lines(file_path: impl AsRef<Path>) -> io::Result<Vec<String>> {
     let file = File::open(file_path)?;
-    Ok(io::BufReader::new(file).lines())
+    io::BufReader::new(file).lines().collect()
 }
